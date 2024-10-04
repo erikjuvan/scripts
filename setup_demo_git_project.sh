57f0eb29f613672eddd7e582829d666246f83552
erik.juvan <erik.juvan@gorenje.com>
2024-10-04 11:30:16 +0200

    setup_demo_git_project.sh don't create dir if it exists. Improve commits

--- setup_demo_git_project.sh
+++ setup_demo_git_project.sh
@@ -11,8 +11,14 @@ set -e # Exit immediately if a pipeline (see Pipelines), which may consist of a 
 target_directory=.
 # Check if a directory argument is provided
 if [ $# -eq 1 ]; then
-  target_directory=$1
-  mkdir -p "$target_directory"
+    target_directory=$1
+
+    if [ -d "$target_directory" ]; then
+        echo "[ERROR] Directory '$target_directory' already exists."
+        exit 1
+    else
+        mkdir -p "$target_directory"
+    fi
 fi
 # Change the working directory to the provided directory
 cd "$target_directory" || exit
@@ -30,7 +36,18 @@ local_dir=$base_dir/$project_dirname
 remote_dir=$base_dir/$remote_dirname
 
 # Create necessary directories
-mkdir -p "$local_dir" "$remote_dir/origin" "$remote_dir/github"
+if [ -d "$local_dir" ]; then
+    echo "[ERROR] Directory '$local_dir' already exists."
+    exit 1
+elif [ -d "$remote_dir/origin" ]; then
+    echo "[ERROR] Directory '$remote_dir/origin' already exists."
+    exit 1
+elif [ -d "$remote_dir/github" ]; then
+    echo "[ERROR] Directory '$remote_dir/github' already exists."
+    exit 1
+else
+    mkdir -p "$local_dir" "$remote_dir/origin" "$remote_dir/github"
+fi
 
 # Define what we will use for autocrlf
 # Git can handle this by auto-converting CRLF line endings into LF when you add a file to the index, and vice versa when it checks out code onto your filesystem. You can turn on this functionality with the core.autocrlf setting. If you’re on a Windows machine, set it to true — this converts LF endings into CRLF when you check out code:
@@ -43,7 +60,7 @@ mkdir -p "$local_dir" "$remote_dir/origin" "$remote_dir/github"
 # If you’re a Windows programmer doing a Windows-only project, then you can turn off this functionality, recording the carriage returns in the repository by setting the config value to false:
 # $ git config --global core.autocrlf false
 #
-# Since this is mainly used on MSYS, I will use the input - "linux on windows" setting. Change it if needed.   
+# Since this is mainly used on MSYS, I will use the input - "linux on windows" setting. Change it if needed.
 core_autocrlf=input
 
 function create_git_repo() {
@@ -154,46 +171,101 @@ git -c protocol.file.allow=always fetch --all
 git submodule foreach --recursive 'git -c protocol.file.allow=always fetch --all'
 
 # Additional steps (commits)
-read -n 1 -r -p "Make some commits to main and develop to make them dirty [Y/n]? "
-echo
-if [[ $REPLY = [Yy] ]]; then
-    # Add your commit steps here...
+read -n 1 -r -p "Make commits to main and develop to have something to push to remotes [Y/n]? "
+if [[ ! $REPLY =~ [Nn] ]]; then
     echo "Adding commits to simulate changes..."
-    
-    # Create new commits on main and develop branch
-    cd "${local_dir}"/release/safe
-    git checkout main
-    echo "Some safe stuff" > safe.c
-    git add safe.c
-    git commit -am "Add safe.c"
-    git tag v1.0.0
-    git checkout -b develop main
-    echo "Some safe develop stuff" >> safe.c
-    git add safe.c
-    git commit -am "Add safe.c develop stuff"
-    git tag v1.1.0-rc
 
-    cd ../user
-    git checkout main
-    echo "Some user stuff" > user.c
-    git add user.c
-    git commit -am "Add user.c"
-    git tag v1.1.0
-    git checkout -b develop main
-    echo "Some user develop stuff" >> user.c
-    git add user.c
-    git commit -am "Add user.c develop stuff"
-    git tag v1.2.0-rc
+    # safe/shared
+    (
+        echo "[INFO] Working on safe/shared..."
+        cd safe/submodules/shared
+        git checkout main
+        echo "Some shared stuff" > shared.c
+        git add shared.c
+        git commit -am "Add shared.c"
+        git checkout -b develop main
+        echo "Some shared develop stuff" >> shared.c
+        git add shared.c
+        git commit -am "Add shared.c develop stuff"
+        # Issue: with git log --oneline --graph --all the log does not output the branch names
+        # The issue you're seeing is likely because the command substitution used with exec affects how git log is determining the output terminal's capabilities. When you use exec > >(tee "tmp_log.log"), git log may think it's not outputting to an interactive terminal (TTY), which changes its formatting behavior (like omitting branch names and colors).
+        git log --oneline --graph --all --decorate # To make sure git log outputs the branch names correctly, you can force it to treat the output as if it's going to a terminal by adding the --decorate
+    )
 
-    cd ../safe/submodules/shared
-    git checkout main
-    echo "Some shared stuff" > shared.c
-    git add shared.c
-    git commit -am "Add shared.c"
-    git checkout -b develop main
-    echo "Some shared develop stuff" >> shared.c
-    git add shared.c
-    git commit -am "Add shared.c develop stuff"
+    # safe
+    (
+        echo "[INFO] Working on safe..."
+        cd safe || exit
+        git checkout main
+        echo "Some safe stuff" > safe.c
+        git add safe.c
+        git commit -m "Add safe.c"
+        git tag v1.0.0
+        git checkout -b develop main
+        echo "Some safe develop stuff" >> safe.c
+        git add safe.c
+        git commit -m "Edit safe.c"
+        git tag v1.1.0-rc
+        git add submodules/shared
+        git commit -am "Update shared"
+        git tag v1.2.0-rc
+        git log --oneline --graph --all --decorate
+    )
+
+    # user/simulink
+    (
+        echo "[INFO] Working on user/simulink..."
+        cd user/submodules/simulink
+        git checkout main
+        echo "Some simulink stuff" > simulink.c
+        git add simulink.c
+        git commit -m "Add simulink.c"
+        git tag v0.1.1
+        git checkout -b develop main
+        echo "Some simulink develop stuff" >> simulink.c
+        git add simulink.c
+        git commit -m "Edit simulink.c"
+        git tag v0.1.2-rc
+        git log --oneline --graph --all --decorate
+    )
+
+    # user
+    (
+        echo "[INFO] Working on user..."
+        cd user
+        git checkout main
+        echo "Some user stuff" > user.c
+        git add user.c
+        git commit -m "Add user.c"
+        git tag v0.1.0
+        git checkout -b develop main
+        echo "Some user develop stuff" >> user.c
+        git add user.c
+        git commit -m "Edit user.c"
+        git tag v1.0.0-rc
+        git add submodules/simulink
+        git commit -am "Update simulink"
+        git tag v1.1.0-rc
+        git log --oneline --graph --all --decorate
+    )
+
+    # release
+    (
+        echo "[INFO] Working on release..."
+        git checkout main
+        echo "#!/bin/bash" > release.sh
+        git add release.sh
+        git commit -m "Add release.sh"
+        git tag v1.0.0
+        git add user
+        git commit -m "Update USER"
+        git checkout -b develop main
+        git add safe
+        git commit -m "Update SAFE"
+        git tag v2.0.0-rc
+        git log --oneline --graph --all --decorate
+    )
+
 fi
 
 echo "Git playground setup finished"
