473b8f79d4fb489defdd5f2b50e695d1782366e2
erik.juvan <erik.juvan@gorenje.com>
2024-10-04 11:44:36 +0200

    Do not switch between branches, do not pull, only push, do not check for dirty repo. Add ability to exclude repo.
    Do not switch between branches, do not pull, only push, do not check for dirty repo (allow pushing with dirty repo). Add ability to skip pushing if repo name contains string (e.g. skip pushing to repo whose name contains simulink).

--- push_branches_tags_to_remotes.sh
+++ push_branches_tags_to_remotes.sh
@@ -6,13 +6,16 @@
 # To use this script with the demo git playground set up by setupt_demo_git_projects.sh
 # add -c protocol.file.allow=always to git commands that operate on remote repos (fetch, clone, push, pull, ...)
 
+# https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
 set -x # Print a trace of simple commands, for commands, case commands, select commands, and arithmetic for commands and their arguments or associated word lists after they are expanded and before they are executed.
 set -e # Exit immediately if a pipeline (see Pipelines), which may consist of a single simple command (see Simple Commands), a list (see Lists of Commands), or a compound command (see Compound Commands) returns a non-zero status.
 
 # Log the process to a temporary log file which will get moved on EXIT trap set up down below.
 # We are logging to a temporary file because I want the log file to reside in the target_directory.
 # But if I directly place it there the repo gets dirty, so instead it is placed in a tmp/ folder
-# and on EXIT trap gets copied over.
+# and on EXIT trap gets copied over. EDIT: This is not necessary anymore since I removed
+# the check for a dirty git repo because I'm no longer switching between branches, but I'll leave it
+# anyway as an example of how it can be done, since it's not really hurting anything.
 temp_log_file=$(mktemp)
 exec > >(tee "$temp_log_file") 2>&1 # Redirect stdout (1) and stderr (2) to both the terminal and the log file
 
@@ -42,39 +45,43 @@ function git_is_clean() {
 function git_push_branches_tags_to_remotes() {
     remotes=(github origin)
     branches=(develop master main)
+    exclusions=("simulink")
 
     relative_path=$(realpath -s --relative-to="$absolute_target_directory" "$PWD")
+    repo_name=$(basename "$relative_path")
 
-    printf "[INFO] Repository: '%s'\n" "$relative_path" # NOTE In the context of git submodule foreach, $path is set by git to the path of each submodule. Currently though this function is being called by bash through xargs, so this path is not directly from git submodule but instead from xargs calling bash and bash exporting it and then calling this function.
+    # Check if the repository name contains any of the exclusions
+    for exclusion in "${exclusions[@]}"; do
+        if [[ "$repo_name" == *"$exclusion"* ]]; then
+            echo "[INFO] Repository '$relative_path' is excluded. Skipping..."
+            return
+        fi
+    done
+
+    echo "[INFO] Repository: '$relative_path'" # NOTE In the context of git submodule foreach, $path is set by git to the path of each submodule. Currently though this function is being called by bash through xargs, so this path is not directly from git submodule but instead from xargs calling bash and bash exporting it and then calling this function.
 
     for remote in "${remotes[@]}" ; do
+        # Push branches
         for branch in "${branches[@]}" ; do
-
-            echo "[INFO] Pushing ${remote}/${branch}"
-            
             # if branch exists
-            if [ "$(git rev-parse --verify "$branch" 2>/dev/null)" ]; then
-                # Save the current branch name
-                current_branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+            if git show-ref --verify --quiet "refs/heads/$branch"; then
+                # I don't think this is needed if we are not changing branches
+                # # Check if we have uncommitted changes
+                # if ! git_is_clean; then
+                #     echo "[ERROR] Uncommitted changes present. Skipping branch $branch."
+                #     continue
+                # fi
 
-                # Check if we are on a branch
-                if [ -z "$current_branch" ]; then
-                    echo "[ERROR] Not on a branch. Skipping..."
-                    return 1
+                # git -c protocol.file.allow=always pull --rebase "$remote" "$branch" # I don't think a pull is needed
+
+                echo "[INFO] Pushing ${remote}/${branch}"
+                # If push fails that is not necessarily an error since a submodule of one project can be on a different
+                # branch and be behind the other one that was already pushed, but that is not an error.
+                if ! git -c protocol.file.allow=always push "$remote" "$branch"; then
+                    echo "[WARNING] Failed to push $remote/$branch on $relative_path"
                 fi
-
-                # Check if we have no uncommitted changes
-                if ! git_is_clean; then
-                    echo "[ERROR] Uncommitted changes present. Skipping branch $branch."
-                    continue
-                fi
-
-                git checkout "$branch"
-                git -c protocol.file.allow=always pull "$remote" "$branch"
-                git -c protocol.file.allow=always push "$remote" "$branch"
-
-                # Checkout the saved branch
-                git checkout "$current_branch"
+            else
+                echo "[INFO] Branch '$branch' does not exist. Skipping..."
             fi
         done
 
@@ -98,19 +105,24 @@ push_branches_tags_to_all_submodules_in_reverse_order() {
 
 # Push branches and tags for all submodules but do it in reverse order since I think that if you try to push
 # a branch and the submodules aren't pushed the push will fail.
-echo "[INFO] 1st Push all submodule's branches and tags to remotes. Important: submodules are in reverse order!"  
+echo "[INFO] 1st Push all submodule's branches and tags to remotes. Important: submodules are in reverse order!"
 push_branches_tags_to_all_submodules_in_reverse_order
 
 # Fetch all changes after the push
-echo "[INFO] Submodules 'fetch --all'"  
+echo "[INFO] Submodules 'fetch --all'"
 git submodule foreach --recursive 'git -c protocol.file.allow=always fetch --all'
 
-# Run this twice in case submodule was committed in a repo that gets evaluated after another one
-echo "[INFO] 2nd Push all submodule's branches and tags to remotes. Important: submodules are in reverse order!"  
+# Run this twice in case submodule was committed in a repo that gets evaluated after another one.
+# Detailed explanation. Lets say user/submodules/shared is first in line to get pushed, then follows
+# safe/submodules/shared. Now let's also say that the actual changes to the shared submodule were done
+# in safe/submodules/shared. So with the 1st round of pushing the user/submodules/shared is left unchanged.
+# Hmm... this still feels OK, since we are fetching after the first push. I fail to see what the second
+# push would account for, but ChatGPT seems to think it's a good idea, so I'll keep it.
+echo "[INFO] 2nd Push all submodule's branches and tags to remotes. Important: submodules are in reverse order!"
 push_branches_tags_to_all_submodules_in_reverse_order
 
 # Now also push the main repo after all the submodules have been pushed
-echo "[INFO] Main repository (superproject): push all branches and tags to remotes."  
+echo "[INFO] Main repository (superproject): push all branches and tags to remotes."
 git_push_branches_tags_to_remotes
 
 echo "[INFO] Pulling and pushing finished."
