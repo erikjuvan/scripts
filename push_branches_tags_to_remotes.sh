d239feac9c4d7b97ecb84b39e0ba2400b6fc8c70
erik.juvan <erik.juvan@gorenje.com>
2024-10-07 14:03:24 +0200

    Clean up push_branches_tags_to_remotes.sh

--- push_branches_tags_to_remotes.sh
+++ push_branches_tags_to_remotes.sh
@@ -4,7 +4,10 @@
 # It will go through all the submodules and push to all remotes all the branches
 # defined in the function below. It will also push all the tags
 # To use this script with the demo git playground set up by setupt_demo_git_projects.sh
-# add -c protocol.file.allow=always to git commands that operate on remote repos (fetch, clone, push, pull, ...)
+# add -c protocol.file.allow=always to git commands that cause issue with error 'fatal: transport 'file' not allowed'
+
+# Set the debug trace to include line numbers
+export PS4='+ ${LINENO}: '
 
 # https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
 set -x # Print a trace of simple commands, for commands, case commands, select commands, and arithmetic for commands and their arguments or associated word lists after they are expanded and before they are executed.
@@ -37,11 +40,6 @@ cd "$target_directory" || exit
 # Save the absolute location of the target directory
 absolute_target_directory=$(realpath "$PWD")
 
-function git_is_clean() {
-    # Check if there are any uncommitted changes or untracked files
-    git diff-index --quiet HEAD -- && git diff --cached --quiet && return 0 || return 1
-}
-
 function git_push_branches_tags_to_remotes() {
     remotes=(github origin)
     branches=(develop master main)
@@ -58,22 +56,13 @@ function git_push_branches_tags_to_remotes() {
         fi
     done
 
-    echo "[INFO] Repository: '$relative_path'" # NOTE In the context of git submodule foreach, $path is set by git to the path of each submodule. Currently though this function is being called by bash through xargs, so this path is not directly from git submodule but instead from xargs calling bash and bash exporting it and then calling this function.
+    echo "[INFO] Repository: '$relative_path'"
 
     for remote in "${remotes[@]}" ; do
         # Push branches
         for branch in "${branches[@]}" ; do
             # if branch exists
             if git show-ref --verify --quiet "refs/heads/$branch"; then
-                # I don't think this is needed if we are not changing branches
-                # # Check if we have uncommitted changes
-                # if ! git_is_clean; then
-                #     echo "[ERROR] Uncommitted changes present. Skipping branch $branch."
-                #     continue
-                # fi
-
-                # git -c protocol.file.allow=always pull --rebase "$remote" "$branch" # I don't think a pull is needed
-
                 echo "[INFO] Pushing ${remote}/${branch}"
                 # If push fails that is not necessarily an error since a submodule of one project can be on a different
                 # branch and be behind the other one that was already pushed, but that is not an error.
@@ -103,26 +92,19 @@ push_branches_tags_to_all_submodules_in_reverse_order() {
     done <<< "$submodule_paths"
 }
 
-# Push branches and tags for all submodules but do it in reverse order since I think that if you try to push
-# a branch and the submodules aren't pushed the push will fail.
-echo "[INFO] 1st Push all submodule's branches and tags to remotes. Important: submodules are in reverse order!"
-push_branches_tags_to_all_submodules_in_reverse_order
-
-# Fetch all changes after the push
-echo "[INFO] Submodules 'fetch --all'"
+# Fetch all
+echo "[INFO] Fetching all."
+git -c protocol.file.allow=always fetch --all
 git submodule foreach --recursive 'git -c protocol.file.allow=always fetch --all'
 
-# Run this twice in case submodule was committed in a repo that gets evaluated after another one.
-# Detailed explanation. Lets say user/submodules/shared is first in line to get pushed, then follows
-# safe/submodules/shared. Now let's also say that the actual changes to the shared submodule were done
-# in safe/submodules/shared. So with the 1st round of pushing the user/submodules/shared is left unchanged.
-# Hmm... this still feels OK, since we are fetching after the first push. I fail to see what the second
-# push would account for, but ChatGPT seems to think it's a good idea, so I'll keep it.
-echo "[INFO] 2nd Push all submodule's branches and tags to remotes. Important: submodules are in reverse order!"
+# Push branches and tags for all submodules but do it in reverse order since I think that if you try to push
+# a branch and the submodules aren't pushed the push will fail. Although it seems to not fail at least not
+# on local filesystem, git daemon, gitea, github, maybe on gitlab?
+echo "[INFO] Push submodules in reverse order!"
 push_branches_tags_to_all_submodules_in_reverse_order
 
 # Now also push the main repo after all the submodules have been pushed
-echo "[INFO] Main repository (superproject): push all branches and tags to remotes."
+echo "[INFO] Push main repository."
 git_push_branches_tags_to_remotes
 
-echo "[INFO] Pulling and pushing finished."
+echo "[INFO] Done."
